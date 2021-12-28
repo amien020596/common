29d67b3c1069b5634618c8eb920d3f4c8f582d47
amien kurniawan <offdevamienk@gmail.com>
2021-12-28 13:43:10 +0700

    init

---
+++ src/InfluencerScope.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Microservices;
+
+use Closure;
+use Illuminate\Auth\AuthenticationException;
+
+class InfluencerScope
+{
+    private $userService;
+    public function __construct(UserServices $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        if ($this->userService->isInfluencer()) {
+            return $next($request);
+        }
+        throw new AuthenticationException;
+    }
+}
